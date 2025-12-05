1a4a91051fd9c48903e7f38b69ea8366df7f9d8f
MohnishKJ <mohnishkj@gmail.com>
2025-12-05 11:35:59 +0530

    Prepared backend for render  deployment

--- backend/app.py
+++ backend/app.py
@@ -276,5 +276,6 @@ if __name__ == '__main__':
 
 # Production-safe server
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
